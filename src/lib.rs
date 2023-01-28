de982ec5206c57fc5f570e97396e37039a88614c
Aidan Neal <decator.c@proton.me>
2023-01-28 01:34:26 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use serenity::{builder::CreateApplicationCommand, model::prelude::command::CommandOptionType};
+use truncrate::*;
+use wikipedia_api::*;
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command
+        .name("wiki")
+        .description("Get a summary of a topic from wikipedia.org")
+        .dm_permission(true)
+        .create_option(|option| {
+            option
+                .name("title")
+                .description("The title to search wikipedia.org for")
+                .kind(CommandOptionType::String)
+                .required(true)
+        })
+}
+
+pub async fn run(title: String, max: usize) -> String {
+    let page = match Page::search(&title).await {
+        Ok(x) => x,
+        Err(e) => return e.to_string(),
+    };
+    let mut content = match page.get_summary().await {
+        Ok(x) => x,
+        Err(e) => return e.to_string(),
+    };
+    if content.len() >= max {
+        content = format!("{}...", content.truncate_to_boundary(max));
+    }
+
+    format!("{}\n{}", content, page.get_url())
+}
